e76f5930d068b1b8b6b449214a6b9c98e16ea153
AlexIbby <alex.a.ibrahim@gmail.com>
2025-05-26 09:11:35 -0400

    categories strengths by week renaming

--- main.py
+++ main.py
@@ -475,4 +475,4 @@ def api_bulk_matchups():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')   
